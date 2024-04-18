8e72dc6310a75c42d8ddf445a0864e10e6f4a573
Moteeullah Azmi <you@example.com>
2024-04-18 15:51:51 +0530

    changes mongodb online to server

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const mongoose =require("mongoose");
+require('dotenv').config()
 
 // define the mongoDB connection URL
-const mongoURL = "mongodb://localhost:27017/hotel"
+// const mongoURL = process.env.MONGO_URL_LOCAL
+const mongoURL = process.env.MONGO_URL;
 //hotel is the database name
 
 // setup MongoDB connection
