38c68643d1889fc999300759263ef51dd415e2ea
Moteeullah Azmi <you@example.com>
2024-04-19 11:14:38 +0530

    last

--- db.js
+++ db.js
@@ -1,29 +1,33 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 // Define the MongoDB connection URL
+// const mongoURL = process.env.MONGODB_URL_LOCAL // Replace 'mydatabase' with your database name
 const mongoURL = process.env.MONGO_URL;
 
-// Setup MongoDB connection
+// Set up MongoDB connection
 mongoose.connect(mongoURL, {
-    useNewUrlParser: true, // Deprecated, but harmless
-    useUnifiedTopology: true // Deprecated, but harmless
-});
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
 // Get the default connection
+// Mongoose maintains a default connection object representing the MongoDB connection.
 const db = mongoose.connection;
 
+// Define event listeners for database connection
+
 db.on('connected', () => {
-    console.log("MongoDB connection Connected");
+    console.log('Connected to MongoDB server');
 });
 
 db.on('error', (err) => {
-    console.log("MongoDB connection Error:", err);
+    console.error('MongoDB connection error:', err);
 });
 
 db.on('disconnected', () => {
-    console.log("MongoDB connection Disconnected");
+    console.log('MongoDB disconnected');
 });
 
-// Export database 
+// Export the database connection
 module.exports = db;
